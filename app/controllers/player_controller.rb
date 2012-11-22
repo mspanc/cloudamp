791f32234a01596b8b5a52ae6412f32d28aadb06
Marcin Lewandowski <marcin@saepia.net>
2012-11-22 18:06:51 +0100

    Started working on player

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -1,2 +1,16 @@
 class PlayerController < ApplicationController
+  before_filter :authenticate_user
+
+  def main
+
+  end
+  
+  
+  
+  protected
+  
+  def authenticate_user
+    
+    # TODO
+  end
 end
