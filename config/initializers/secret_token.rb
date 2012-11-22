5edebe46a55f3c9f34ca2bffa4496b4e5cea8875
Marcin Lewandowski <marcin@saepia.net>
2012-11-22 15:01:09 +0100

    Initial RoR commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cloudamp::Application.config.secret_token = 'f843503cde9aedd7006fde2a3ab9e7b3e0c0f61d7f895dec58e0c584d8d8a10036559e0bdb73ef5ed28b233b2963d0cac751c69f4811999bce19dec6ab8cf5fd'
