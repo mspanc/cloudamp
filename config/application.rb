a7c120e7c2dc8dfa8f21278ab3a0b6e48f5efe02
Marcin Lewandowski <marcin@saepia.net>
2012-11-26 00:14:57 +0100

    Disabled asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -56,7 +56,7 @@ module Cloudamp
 
 
     # Enable the asset pipeline
-    config.assets.enabled = true
+    config.assets.enabled = false
     config.assets.initialize_on_precompile = false
     
     # Version of your assets, change this if you want to expire all your assets
