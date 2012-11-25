cb06fa6005b5054570e430a62f75ac5487df90f0
Marcin Lewandowski <marcin@saepia.net>
2012-11-26 00:06:29 +0100

    Config tweaks for heroku

--- config/application.rb
+++ config/application.rb
@@ -53,9 +53,12 @@ module Cloudamp
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
 
+
+
     # Enable the asset pipeline
     config.assets.enabled = true
-
+    config.assets.initialize_on_precompile = false
+    
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
