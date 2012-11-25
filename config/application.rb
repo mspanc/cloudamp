c3900f7703d3a332d9c407bfc50ac5693775817e
Marcin Lewandowski <marcin@saepia.net>
2012-11-26 00:20:16 +0100

    Updates to asset pipeline conf

--- config/application.rb
+++ config/application.rb
@@ -56,8 +56,11 @@ module Cloudamp
 
 
     # Enable the asset pipeline
-    config.assets.enabled = false
-    config.assets.initialize_on_precompile = false
+    config.assets.enabled = true
+    config.assets.initialize_on_precompile = true
+
+    config.assets.precompile << %w( welcome.css player.css )
+    config.assets.precompile << %w( player.js )
     
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
