18c27e0c867c99866321c1ff9a6730dba901fc9c
Marcin Lewandowski <marcin@saepia.net>
2012-11-26 22:52:06 +0100

    Make sessions valid for a month

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Cloudamp::Application.config.session_store :cookie_store, key: '_cloudamp_session'
+Cloudamp::Application.config.session_store :cookie_store, key: '_cloudamp_session', expire_after: 30.days
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
