084e79f109b373430eadfdffcb8feaf7a8397d8f
Marcin Lewandowski <marcin@saepia.net>
2012-11-27 12:47:28 +0100

    Using different SC app for dev environment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,11 @@ class ApplicationController < ActionController::Base
   end
   
   
+  # Returns current user.
+  #
+  # It is found based on session data.
+  #
+  # Ensures that it is initialized only once. 
   def current_user
     return @current_user if @current_user
     
@@ -31,19 +36,31 @@ class ApplicationController < ActionController::Base
       nil
   end
   
+  
+  # Returns true if user is signed in
   def user_signed_in?
     not current_user.nil?
   end
 
+
+  # Before filter that ensures user is authenticated.
+  #
+  # It always returns 403 forbidden error if user is not signed in.
   def authenticate_user_with_forced_forbidden
     perform_authenticate_user true
   end
-  
+
+
+  # Before filter that ensures user is authenticated.
+  #
+  # It always 403 forbidden error for JSON requests and redirects
+  # to login screen for HTML requests.  
   def authenticate_user
     perform_authenticate_user false
   end
   
   
+  # Helper function that does actual authentication in before filters.
   def perform_authenticate_user(forced_forbidden)
     unless user_signed_in?
       # Clean up the session 
